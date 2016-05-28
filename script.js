e014748493d622e17720644e177e1971d08d0848
geoff loh <geoffreyloh@gmail.com>
2016-05-29 00:22:06 +1200

    Otatou website

---
+++ script.js
@@ -0,0 +1,17 @@
+/*document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " OTATOU Innovations. All rights reserved.</p>";*/
+
+document.getElementById("header").innerHTML =
+		"<ul id='top'>" +
+		"<li><a href='/'><img src='img/home.png'></a></li>" +
+		"<li style='float:right'><a href='about.html'><img src='img/contact.png'></a></li>" +
+		"</ul>";
+		
+
+document.getElementById("nav01").innerHTML =
+		"<ul id='menu'>" +
+		"<li><a href='cleversplit.html'><img src='img/clever.png'></a></li>" +
+		"<li><a href='onemanstrash.html'><img src='img/trash.png'></a></li>" +
+		"<li><a href='novel.html'><img src='img/evo.png'></a></li>" +
+		"<li><a href='tradiebin.html'><img src='img/bin.png'></a></li>" +
+		"</ul>";
