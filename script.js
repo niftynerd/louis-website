7fc31fa46bf2c44e42aa835523305038d1268de4
geoff loh <geoffreyloh@gmail.com>
2016-05-29 12:45:13 +1200

    Update

--- script.js
+++ script.js
@@ -2,16 +2,25 @@
 "<p>&copy;  " + new Date().getFullYear() + " OTATOU Innovations. All rights reserved.</p>";*/
 
 document.getElementById("header").innerHTML =
-		"<ul id='top'>" +
+		"<img id='page_head_image' src='img/page_header.png' usemap='#logos' style='width:100%; border:none;'>" +
+		"<map id='page_head_map' name='logos'>" +
+		"<area shape='rect' coords='15,17,214,97' href='/' title='Home' alt='home' />" +
+		"<area shape='rect' coords='859,10,983,97' href='about.html' title='About' alt='about' />" +
+		"<area shape='rect' coords='34,110,298,190' href='cleversplit.html' title='Clever Split' alt='cleversplit' />" +
+		"<area shape='rect' coords='298,110,520,190' href='onemanstrash.html' title='One Man's Trash' alt='onemanstrash' />" +
+		"<area shape='rect' coords='520,110,748,190' href='novel.html' title='Evolution Of A Conscious Mind' alt='evolutionofaconsciousmind' />" +
+		"<area shape='rect' coords='748,110,997,190' href='tradiebin.html' title='Tradie Bin' alt='tradiebin' />" +
+		"</map>";
+		/*"<ul id='top'>" +
 		"<li><a href='/'><img src='img/home.png'></a></li>" +
 		"<li style='float:right'><a href='about.html'><img src='img/contact.png'></a></li>" +
-		"</ul>";
+		"</ul>";*/
 		
 
-document.getElementById("nav01").innerHTML =
+/*document.getElementById("nav01").innerHTML =
 		"<ul id='menu'>" +
 		"<li><a href='cleversplit.html'><img src='img/clever.png'></a></li>" +
 		"<li><a href='onemanstrash.html'><img src='img/trash.png'></a></li>" +
 		"<li><a href='novel.html'><img src='img/evo.png'></a></li>" +
 		"<li><a href='tradiebin.html'><img src='img/bin.png'></a></li>" +
-		"</ul>";
+		"</ul>";*/
